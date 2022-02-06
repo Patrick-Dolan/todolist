8847a0d1543806165ae9f3ba2c075462baec5dca
Patrick Dolan <dolanp1992@gmail.com>
2022-02-06 10:57:31 -0800

    add console ui

---
+++ ToDoList.Solution/ToDoList/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to your To Do List.");
+      string userAnswer = "add";
+
+      while (userAnswer == "add" || userAnswer == "view")
+      {
+      Console.WriteLine("Would you like to add an item to your list or view you list? (Add/View)");
+      userAnswer = Console.ReadLine().ToLower();
+        if (userAnswer == "add")
+        {
+        Console.WriteLine("Please enter a description for the new item:");
+        Item newItem = new Item(Console.ReadLine());
+        Console.WriteLine(" '" + newItem.Description + "' " + "has been added to your list.");
+        } 
+        else if (userAnswer == "view")
+        {
+          List<Item> list = new List<Item> {};
+          list = Item.GetAll();
+          if (list.Count == 0)
+          {
+            Console.WriteLine("You don't have any items on your list.");
+          }
+          foreach (Item item in list)
+          {
+            Console.WriteLine(item.Description);
+          }
+        }
+      }
+      Console.WriteLine("Shutting down application now. Goodbye.");
+    }
+  }
+}
